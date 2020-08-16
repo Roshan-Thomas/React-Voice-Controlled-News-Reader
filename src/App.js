2c30cfb3aada81d6b38042a6d01c4dd1c0f7c7b5
Roshan Thomas <roshan.thomas@qatar.tamu.edu>
2020-08-16 15:51:47 +0300

    created components to display news articles

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
 
+import NewsCards from './components/NewsCards/NewsCards';
+
 const alanKey = '365d99ef683d8a0452822b47a3df97c92e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 const App = () => {
+
+    const [newsArticles, setNewsArticles] = useState([]);
 
     useEffect(() => {
         alanBtn({
             key: alanKey,
             onCommand: ({ command, articles }) => {
                 if(command === 'newHeadlines') {
-                    console.log(articles);
+                    setNewsArticles(articles);
                 }
             }
         })
@@ -19,6 +23,7 @@ const App = () => {
     return (
         <div>
             <h1>Alan AI News Application</h1>
+            <NewsCards articles={newsArticles} />
         </div>
     )
 }
