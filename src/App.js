ceb196544de3908a6857e40868c21e689f7fb430
Roshan Thomas <roshan.thomas@qatar.tamu.edu>
2020-08-16 15:40:54 +0300

    added Alan AI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
+
+const alanKey = '365d99ef683d8a0452822b47a3df97c92e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 const App = () => {
+
+    useEffect(() => {
+        alanBtn({
+            key: alanKey,
+            onCommand: ({ command, articles }) => {
+                if(command === 'newHeadlines') {
+                    console.log(articles);
+                }
+            }
+        })
+    })
+
     return (
         <div>
             <h1>Alan AI News Application</h1>
